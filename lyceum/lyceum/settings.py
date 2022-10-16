20b06e5507773e61650a0fc6e65e8e0aa5d97129
GrishaTS <bezgrisa@gmail.com>
2022-10-17 01:24:32 +0300

    Задание 1 урок1

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homapage.apps.HomepageConfig',
+    'homepage.apps.HomepageConfig',
     'catalog.apps.CatalogConfig',
     'about.apps.AboutConfig'
 ]
