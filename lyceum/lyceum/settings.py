f8299eaa492c56f73b31b0a9272c0d66f8db0461
GrishaTS <bezgrisa@gmail.com>
2022-10-18 18:35:50 +0300

    Update settings.py

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'DEFAULT_SECRET_KEY')
 
-DEBUG = (os.getenv('DEBUG', 'True') == 'True')
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
