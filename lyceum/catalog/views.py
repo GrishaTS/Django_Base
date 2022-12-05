3cdf76310329786aa9c7e748b5e4cb62bb34c0ff
GrishaTS <bezgrisa@gmail.com>
2022-12-05 22:07:19 +0300

    def -> cbv

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -1,24 +1,16 @@
-from django.shortcuts import get_object_or_404, render
+from django.views.generic import DetailView, ListView
 
 from .models import Item
 
 
-def item_list(request):
+class ItemListView(ListView):
+    model = Item
     template_name = 'catalog/item_list.html'
-    items = Item.objects.published().order_by('category__name', 'name')
-    context = {
-        'items': items,
-    }
-    return render(request, template_name, context)
+    context_object_name = 'items'
+    get_queryset = Item.objects.published
 
 
-def item_detail(request, pk=1):
+class ItemDetailView(DetailView):
+    model = Item
     template_name = 'catalog/item_detail.html'
-    item = get_object_or_404(
-        Item.objects.published(),
-        pk=pk,
-    )
-    context = {
-        'item': item,
-    }
-    return render(request, template_name, context)
+    context_object_name = 'item'
